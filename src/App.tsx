231ec11206ca118ad8460982e0cbce343dc66d27
AmitFikler <amitfikler@gmail.com>
2022-05-01 12:06:58 +0300

    initaial commit react

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className='App'>
+      <div className='div1 data'> </div>
+      <div className='div2 data'> </div>
+      <div className='div3 data'> </div>
+      <div className='div4 data'> </div>
+      <div className='div5 data'> </div>
+      <div className='div6 data'> </div>
+    </div>
+  );
+}
+
+export default App;
